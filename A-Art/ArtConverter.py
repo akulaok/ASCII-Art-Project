ddba948fa14e53b98bf5721a1987650af52548e5
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2023-11-19 01:27:11 +0500

    Add files via upload

--- A-Art/ArtConverter.py
+++ A-Art/ArtConverter.py
@@ -1,5 +1,5 @@
 import cv2
-import pygame as pg
+from PIL import Image, ImageDraw, ImageFont
 import os
 from math import ceil
 from AsciiSorter import AsciiSorter
@@ -8,7 +8,7 @@ from AsciiSorter import AsciiSorter
 class ArtConverter:
     """Этот клас читает изображение и создаёт новое с помощью ASCII-символов"""
 
-    def __init__(self, path, need_preview, font_size, ascii_palette, need_sort_palette, variable_space, font_name):
+    def __init__(self, path, font_size, ascii_palette, need_sort_palette, variable_space, font_name):
         """Конструктор класса"""
         # Проверка на наличие токого файла
         if not os.path.exists(path):
@@ -20,21 +20,21 @@ class ArtConverter:
         if "/" in path:
             print("Нежелательный символ '/' в пути, заменён на '\\'")
             path = path.replace("/", "\\")
-        # Инициализация PyGame
-        pg.init()
         # Путь до файла
         self.path = path
-        self.need_preview = need_preview
         # Имя файла
         self.file_name = path.split("\\")[-1].split(".")[0]
         # Расширение файла
         self.expansion = path.split("\\")[-1].split(".")[1]
+        # Проверка, что входной файл подходт по расширению
+        if self.expansion not in ["jpg"]:
+            raise Exception(f"Расширение {self.expansion} не обрабатывается")
         # Получаем изображение как спсисок списков, состояших из номеров цвета пикселей
         self.image = self.get_image()
         # Получаем размеры изображения
         self.res = self.width, self.height = self.image.shape[0], self.image.shape[1]
-        # Создание поля PyGame, на котором буде рисовать
-        self.surface = pg.display.set_mode(self.res)
+        # Заготовка для изображения PIL
+        self.pil_image = Image.new("RGB", (self.width, self.height), "black")
         # Палитра ascii-символов  (Можно поэксперементировать)
         self.ascii_chars = list(ascii_palette)
         # Обозначение пути до шрифта
@@ -52,12 +52,13 @@ class ArtConverter:
             raise Exception("В палитре не может быть больше 255 разных символов")
         # Средство для определения, какой символ писать
         self.ascii_coefficient = ceil(255 / (len(self.ascii_chars)))
-        # Определение шрифта для PyGame
-        self.font = pg.font.SysFont(self.font_path, self.font_size)
-        # Константа, взятая, чтобы размеры изображений совпадали (Можно поэксперементировать)
+        # Константа, взятая, чтобы размеры изображений совпадали
         self.char_step = int(self.font_size * 0.6)
-        # Рендеринг символов, чтобы их можно было добавить в итоговое изображение
-        self.rendered_ascii_chars = [self.font.render(char, False, "white") for char in self.ascii_chars]
+        # Определение шрифта для PIL и проверка того, что с ним можно работать
+        try:
+            self.font = ImageFont.truetype(self.font_path, self.font_size)
+        except Exception:
+            raise Exception("Возникла ошибка при работе со шрифтом, пожалуйста, выберите другой")
 
     def get_image(self):
         """Этот метод переводит изображение в список списков пикслей"""
@@ -65,47 +66,19 @@ class ArtConverter:
         cv2_image = cv2.imread(self.path)
         # Транспонирование матрицы () иначе изображение перевёрнутое
         transposed_image = cv2.transpose(cv2_image)
-        # Покраска пикселей в серый цвет (от 0 до 255 вроде бы)
+        # Покраска пикселей в серый цвет (от 0 до 255)
         gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2GRAY)
         return gray_image
 
     def draw_converted_image(self):
-        """Этот метод создаёт ASCII-изображение"""
-        # Заполнение поля PyGame тёмным цветом
-        self.surface.fill("black")
-        # Выяснение какой сивол должен быть напечатан вместо конкретного пикселя (Можно поэксперементировать)
+        """Формирование ascii-изображения"""
+        # Присвоение пикселю соответствующую ему букву палитры ascii-символов
         char_indices = self.image // self.ascii_coefficient
-        # Обход исходного изображение и добавления в итоговый результат ASCII-символы в зависимости от char_indices
+        # Стандартная процедура для начала рисования PIL
+        draw = ImageDraw.Draw(self.pil_image)
+        # Обход излбражения с нужным шагом и зарисовка нужных символов для PIL
         for x in range(0, self.width, self.char_step):
             for y in range(0, self.height, self.char_step):
-                char_index = char_indices[x, y]
-                self.surface.blit(self.rendered_ascii_chars[char_index], (x, y))
-
-    def save_image(self):
-        """Этот метод сохраняет изображение"""
-        # Представление изображения PyGame, как список списков пикселей
-        pygame_image = pg.surfarray.array3d(self.surface)
-        # Транспонирование для работы с библиоекой CV2
-        cv2_image = cv2.transpose(pygame_image)
-        # Сохранение файла в папке "output_images"
-        cv2.imwrite("output_images\\" + self.file_name + "_converted." + self.expansion, cv2_image)
-
-    def run(self):
-        """Этот метод запускает работу класса"""
-        #  Начало рисовки
-        self.draw_converted_image()
-        if self.need_preview:
-            # Обновление изображения PyGame
-            pg.display.flip()
-            # Вечный цикл для обработки ввода пользователя
-            while True:
-                for i in pg.event.get():
-                    # Обработка закрытия программы
-                    if i.type == pg.QUIT:
-                        exit()
-                    # Обработка нажатия клавиши "s"
-                    elif i.type == pg.KEYDOWN:
-                        if i.key == pg.K_s:
-                            self.save_image()
-        else:
-            self.save_image()
+                draw.text((x, y), self.ascii_chars[char_indices[x, y]], font=self.font, fill="white")
+        # Сохранение изображения
+        self.pil_image.save("output_images\\" + self.file_name + "_converted." + self.expansion)
--- A-Art/start_conversion.py
+++ A-Art/start_conversion.py
@@ -4,9 +4,7 @@ from ArtConverter import ArtConverter
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, help="Путь до файла")
-    parser.add_argument("-n", "--need_preview", action="store_true",
-                        help="Указать, если нужно только показать результат с возможностью сохранения")
-    parser.add_argument("-f", "--font_size", type=int, default=12, help="Размер шрифта")
+    parser.add_argument("-f", "--font_size", type=int, default=8, help="Размер шрифта")
     parser.add_argument("-p", "--ascii_palette", type=str, default='.,"-~+*:;!vxnm#W&8@',
                         help="Палитра ascii-символов")
     parser.add_argument("-s", "--need_sort_palette", action="store_true",
@@ -15,5 +13,5 @@ if __name__ == "__main__":
                         help="Добавление пробела в палитру ascii-символов")
     parser.add_argument("-c", "--choose_font", default="Roboto-BoldItalic", help="Имя шрифта без расширения и пути")
     arg = parser.parse_args()
-    ArtConverter(arg.path, arg.need_preview, arg.font_size, arg.ascii_palette, arg.need_sort_palette,
-                 arg.variable_space, arg.choose_font).run()
+    ArtConverter(arg.path, arg.font_size, arg.ascii_palette, arg.need_sort_palette,
+                 arg.variable_space, arg.choose_font).draw_converted_image()
